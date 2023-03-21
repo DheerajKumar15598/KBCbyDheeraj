56444f8935d4cd2c83e8a61e1ff4658bac5f2d16
Dheeraj <kuldeeppokhrel101@gmail.com>
2023-03-21 22:11:57 +0530

    KBC Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Quiz from './components/Quiz';
 
 function App() {
+  const [questionNo, setQuestionNo] = useState(1)
+  const [timeout, setTimeout] = useState(false)
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answer:[
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Clothes",
+          correct: false,
+        },
+        {
+          text: "Laptop",
+          correct: false,
+        },
+      ]
+    },
+
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answer:[
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Clothes",
+          correct: false,
+        },
+        {
+          text: "Laptop",
+          correct: false,
+        },
+      ]
+    },
+
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answer:[
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Clothes",
+          correct: false,
+        },
+        {
+          text: "Laptop",
+          correct: false,
+        },
+      ]
+    },
+  ]
+
+  const moneyPyramid = [
+    {id:1, money: "$ 100"},
+    {id:2, money: "$ 200"},
+    {id:3, money: "$ 300"},
+    {id:4, money: "$ 500"},
+    {id:5, money: "$ 1000"},
+    {id:6, money: "$ 2000"},
+    {id:7, money: "$ 4000"},
+    {id:8, money: "$ 8000"},
+    {id:9, money: "$ 16000"},
+    {id:10, money: "$ 32000"},
+    {id:11, money: "$ 64000"},
+    {id:12, money: "$ 125000"},
+    {id:13, money: "$ 250000"},
+    {id:14, money: "$ 500000"},
+    {id:15, money: "$ 1000000"},
+  ].reverse()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+          <Quiz data={data} setTimeout={setTimeout} setQuestionNo={setQuestionNo}/>
+        </div>
+      </div>
+
+      <div className="pyramid">
+        <ul className='moneyList'>
+          {moneyPyramid.map((m) => (
+            <li className={questionNo === m.id ? 'moneyListItem active' : 'moneyListItem'}>
+              <span className='moneyListItemNumber'>{m.id}</span>
+              <span className='moneyListItemAmount'>{m.money}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Quiz = () => {
+  return (
+    <div className='quiz'>
+      <div className="question">What's the best youtube channel?</div>
+      <div className="answers">
+        <div className="answer wrong">Lama Dev</div>
+        <div className="answer">CodeWithHappy</div>
+        <div className="answer">Akshay Saini</div>
+        <div className="answer">Thapa Technical</div>
+      </div>
+    </div>
+  )
+}
+
+export default Quiz
